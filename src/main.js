c6e8f9dc90decae5aa6413a7ea64de11ce284c23
Angela Murray <angelamurray776@outlook.com>
2025-12-16 23:11:50 -0500

    auth0 not working

--- src/main.js
+++ src/main.js
@@ -13,23 +13,23 @@ createApp(App).use(router).mount('#app')
 
 
 
-// Import the Auth0 configuration
-import { domain, clientId } from '../auth_config.json';
+// // Import the Auth0 configuration
+// import { domain, clientId } from './auth/auth_config.json';
 
-// Import the plugin here
-import { Auth0Plugin } from './auth';
+// // Import the plugin here
+// import { Auth0Plugin } from './auth';
 
-// Install the authentication plugin here
-Vue.use(Auth0Plugin, {
-    domain,
-    clientId,
-    onRedirectCallback: (appState) => {
-        router.push(
-            appState && appState.targetUrl
-                ? appState.targetUrl
-                : window.location.pathname,
-        );
-    },
-});
+// // Install the authentication plugin here
+// Vue.use(Auth0Plugin, {
+//     domain,
+//     clientId,
+//     onRedirectCallback: (appState) => {
+//         router.push(
+//             appState && appState.targetUrl
+//                 ? appState.targetUrl
+//                 : window.location.pathname,
+//         );
+//     },
+// });
 
-Vue.config.productionTip = false;
+// Vue.config.productionTip = false;
